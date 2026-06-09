5b9570aae5d88e2e02e73bf5bb3cc8e6555f4a51
FORCEACK <crocodileack2002@gmail.com>
2026-06-09 13:05:10 -0600

    Se agrega el proyecto

---
+++ SOFT-GIRA/src/main/java/SoftGira/SOFTGIRA.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package SoftGira;
+
+/**** @author Luis Angel*/
+public class SOFTGIRA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
